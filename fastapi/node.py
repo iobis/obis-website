9e60030c51938293bef4d35a3903343bb0f4e31d
Pieter Provoost <pieterprovoost@gmail.com>
2025-10-25 14:25:36 -0500

    added quality tab

--- fastapi/node.py
+++ fastapi/node.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 from jinja2 import Environment, FileSystemLoader
 import requests
 import urllib
+from lib import get_quality_statistics, get_statistics
 
 router = APIRouter()
 
@@ -24,8 +25,20 @@ async def node_page(request: Request, node_id: str):
         print(e)
         raise HTTPException(status_code=404, detail="Node not found")
 
+    statistics = get_statistics({
+        "nodeid": node_id
+    })
+
+    quality_statistics = get_quality_statistics({
+        "nodeid": node_id,
+        "dropped": "include",
+        "absence": "include"
+    })
+
     block = templates.get_template("node.html").render(
-        node=node
+        node=node,
+        statistics=statistics,
+        quality_statistics=quality_statistics
     )
 
     return shell_templates.TemplateResponse(
